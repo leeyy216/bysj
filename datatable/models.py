972295addc9d17cbc4de3b151fd675c33c608459
leeyy <627088113@qq.com>
2017-04-06 16:38:47 +0800

    04061638

---
+++ datatable/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Enrollinfo(models.Model):
+	from_sch = models.CharField(max_length = 30)
+	stu_num = models.CharField(max_length = 30)
+	stu_name = models.CharField(max_length = 30)
+	into_sch = models.CharField(max_length = 30)
+	
+	def __str__(self):
+		#retu = "{} {} {} {}".format(from_sch,stu_num,stu_name,into_sch)
+		return self.stu_name
